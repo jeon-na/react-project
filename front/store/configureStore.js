0467cefdd5b70a9a9479cfe1b35cc1d7a3857a27
jeon-na <saey1234@gmail.com>
2022-11-05 16:08:11 +0900

    Fix: prefetch false

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -6,14 +6,18 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from '../reducers';
 import rootSaga from '../sagas';
 
+const loggerMiddleware = ( {dispatch, getStates}) => (next) => (action) => {
+  console.log(action);
+  return next(action);
+}
+
 const configureStore = (context) => {
   console.log(context);
   const sagaMiddleware = createSagaMiddleware();
   const middlewares = [sagaMiddleware];
   const enhancer = process.env.NODE_ENV === 'production'
-    ? compose(applyMiddleware(...middlewares))
-    : composeWithDevTools(
-      applyMiddleware(...middlewares),
+    ? compose(applyMiddleware(sagaMiddleware))
+    : composeWithDevTools(applyMiddleware(sagaMiddleware, loggerMiddleware),
     );
   const store = createStore(reducer, enhancer);
   store.sagaTask = sagaMiddleware.run(rootSaga);
